8e5a9d0d5db35438adf9768bd8007de58b391730
Manelle Nouar <60315534+manellenr@users.noreply.github.com>
2020-03-17 18:40:02 +0100

    Update Mastermind.py

--- Mastermind.py
+++ Mastermind.py
@@ -1,37 +1,41 @@
-#importation de la bibliothèque random pour le tirage aléation du code secret
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 18:36:00 2020
+
+@author: nouar manelle
+"""
 import random, sys
 #création d'une fonction mastermind qui par defaud à quatres chiffres du code secret à trouver et 6 couleurs
 def marstermind(pions= 2, couleurs= 4):
-    i = 1 # premier tirage
+    i = 1 
     secret = [str(random.randint(1,couleurs)) for i in range(pions)]
 # le programme  tire un entier aléatoire en 1 et 4 couleurs et il répète ca 2 fois car ont à définit 2 pions
-#on fait une boucle while qui se répète à l'infini
     while True:
         resultat = list(secret) 
-        gameur = list(input("tirage" + str(i) + ";"))#mettre une chaine de caractères en liste
+        gameur = list(input("tirage" + str(i) + ";"))
 #faut configurer si la personne ne trouve pas le code
         if len(gameur) == 0:
             return "vous n'avez pas réussit"
-        i += 1 #pour recommencer un nouveau tirage si le joueur n'a pas réussit
+        i += 1 
 #ensuite on va compter les pions bien placer et mal placé mais on les initiales à zéro vu que la partie à pas débuter
         bien_place, mal_place = 0,0
         for a, valeur in enumerate(gameur):# pour savoir la position des pions est mal placé
-            if valeur in secret:# si le pion est mal placé dans le code a initialisé à zéro
+            if valeur in secret:
                 mal_place += 1#si le nombre est mal placé on augmente de 1 pour continuer à trouver les autres pions
-                secret[secret.index(valeur)] = '$'#trouver le rang de la valeur dans le code qui sera remplacé par un dollar
+                secret[secret.index(valeur)] = '$'
         for b, valeur in enumerate(gameur):# pour savoir la position des pions est bien placé
-          if valeur == resultat[b]: # si le pion est bien placé dans le code b initialisé à zéro
-              bien_place += 1 #si le nombre est bien placé on augmente de 1 pour continuer à trouver les autres pions
-              gameur[i] = '♟'#on remplace le joueur par un symbole
+          if valeur == resultat[b]:
+              bien_place += 1 
+              gameur[i] = '♟'
               resultat[i] = '$'
-          if  bien_place == 2: #si on à 2 pions bien placé on gagne 
+          if  bien_place == 2: 
               return 'youpi! tu as trouvé le secret'
         print(bien_place,":gagner",mal_place,":perdu")
          
 marstermind()        
 
 longueur = 2
-nb_tentative = 4 #4 tentatives maximum pour trouver le code secret
+nb_tentative = 4 
 couleurs = ['bleu','rose','rouge','jaune']#liste des couleurs possibles
 col = [couleurs[i][0] for i in range(len(couleurs))]
 
@@ -41,6 +45,6 @@ def afficher_couleurchoisi():
 afficher_couleurchoisi()
 
 def result_secret():
-    code = '' #le résultat secret renvoie une chaine de chractère
+    code = '' #le résultat secret renvoie une chaine de chractères
     return code    
 result_secret()
