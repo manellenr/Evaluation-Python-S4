58d6367b24d43ef6b0e25ef64a5ba4737a377221
Colin Goutte <cgte@bk.ru>
2020-03-24 22:58:06 +0100

    Feedback

--- mastermind.py
+++ mastermind.py
@@ -6,108 +6,129 @@ Created on Tue Mar 18 00:25 2020
 """
 ################################################ Programme principal ################################################
 
+
 import random
 
+# Non ca c'est dans if __name__ == __main__
 print(" Bienvenue ! ")
 print(" Découvrez le jeu du Mastermind ")
 
 while True:
     partie = input(" Avez-vous déja jouer au Mastermind ? ")
-    if partie == 'oui' or partie == 'non':
+    if partie == "oui" or partie == "non":
         break
 
-while True:
+while True:  # superfliu
     while True:
         level = input(" Choisissez le level : ")
-        if level == 'facile' or level == 'normal' or level == 'difficile':
+        if level == "facile" or level == "normal" or level == "difficile":
             break
-        
-longueur = '2'
-nb_test = '4'
-couleurs = ['bleu','rose', 'or','argent'] 
+
+longueur = "2"
+nb_test = "4"  # ???
+couleurs = ["bleu", "rose", "or", "argent"]
 col = random.choice(couleurs)
 
-def marstermind(pions= 2, couleurs= 4):
-    tour = 1 
-    L = [str(random.randint(1,couleurs)) for a in range(pions)]
+
+def marstermind(pions=2, couleurs=4):  # pions=longueur et coutleurs = couleurs non
+    tour = 1
+    L = [str(random.randint(1, couleurs)) for a in range(pions)]
+    # Bah non vous réinventez la roue.
     while True:
-        resultat = list(L) 
-        gamer = list(input("tirage" + str(tour) + ";"))
+        resultat = list(L)  # Interet "
+        gamer = list(
+            input("tirage" + str(tour) + ";")
+        )  # ????? ça ne vaudra jamais a la ligne du dessous
+
         if len(gamer) == 0:
             return "vous n'avez pas réussi"
-        tour += 1 
+        tour += 1
         bien_place = 0
         mal_place = 0
-        
+        # de ici
         # la position des pions est mal placé
         for a, valeur in enumerate(gamer):
             if valeur in L:
                 mal_place += 1
         # la position des pions bien placé
         for a, valeur in enumerate(gamer):
-          if valeur == resultat[a]:
-              bien_place += 1 
-              gamer[a] = '♟'
-              resultat[a] = '$'
-          elif bien_place == 2: 
-              return 'youpi! tu as trouvé le code'
-        print(bien_place,":gagner",mal_place,":perdu")
-         
-marstermind()
+            if valeur == resultat[a]:
+                bien_place += 1
+                gamer[a] = "♟"  #
+                resultat[a] = "$"
+            elif bien_place == 2:
+                return "youpi! tu as trouvé le code"
+        # à la c'esrt un fonction que vous devez ecrite et utiliser.
+        print(bien_place, ":gagner", mal_place, ":perdu")
 
-def afficher_couleurchoisi():
+
+marstermind()  # __name__ == '__main__'
+
+
+def afficher_couleurchoisi():  # ça ca a toute sa place dans une doctring mais faites plutot des test unitaires.
     for i in couleurs:
         print(i)
+
+
 print(afficher_couleurchoisi())
 
 afficher_couleurchoisi()
 
+
 def result_secret():
-    code_secret = " " 
+    code_secret = " "
     for i in range(longueur):
         position_nb = random.randint(0, len(col) - 1)
         code_secret = col[position_nb] + code_secret
-    return code_secret    
+    return code_secret
+
 
 print(result_secret())
 
-result_secret()  
- 
+result_secret()
+
+# Pourquoi dupliqueer aurant de code .....
+
 
 def evaluation_code(code_secret, code_entree):
     bien_place, mal_place = 0, 0
-    couleurs_secret = list(code_secret) 
+    couleurs_secret = list(code_secret)
     couleurs_choisi = list(code_entree)
-        
-    for v in range(len(couleurs_choisi)):    
+
+    for v in range(len(couleurs_choisi)):
         if couleurs_choisi[v] == couleurs_secret[v]:
             bien_place += 1
-            couleurs_secret[v] = '*'
-            couleurs_choisi[v] = '*'
-            
+            couleurs_secret[v] = "*"
+            couleurs_choisi[v] = "*"
+
     for x in range(len(couleurs_choisi)):
-        if couleurs_choisi[x] in couleurs_secret and couleurs_choisi != '*':
+        if couleurs_choisi[x] in couleurs_secret and couleurs_choisi != "*":
             mal_place += 1
             c = couleurs_choisi[x]
             gamer = couleurs_secret.index(c)
-            couleurs_secret[gamer] = '*'
+            couleurs_secret[gamer] = "*"
     return [bien_place, mal_place]
-    
-print(evaluation_code()) 
+
+
+# voila ca c'st pas mal une contion qui renvoie quelque chose que vous pouvez tester
+
+print(evaluation_code())
 evaluation_code()
 
+
+# nommez vos variables correctectement.
+
+
 def mettre_pions(e, col, longeur):
-     e = input(" mettez vos pions : ")
-     if len(e) != len(longueur):
+    e = input(" mettez vos pions : ")
+    if len(e) != len(longueur):
         print(" vous n'avez mal choisi la longueur des pions ")
         return False
-        for u in e :
-            if u not in col : 
+        for u in e:
+            if u not in col:
                 return False
             else:
                 return e
-    
-mettre_pions()
 
 
-    
+mettre_pions()  # ceci ne risque pas de fonctionner.
--- test_mastermind.py
+++ test_mastermind.py
@@ -1,112 +1,125 @@
-from Mastermind import doctest
+from mastermind import doctest
 import doctest_private_tests_external
 from random import shuffle
 
-#import doctest
+# import doctest
 ################################################ #Doctests ################################################
 
-# doctest testmod   
+# doctest testmod
+
+
+# a quoi sert cette classe ?
+
 
 class level:
     def __init__(self):
         print("init")
+
     def __call__(self):
         print("call")
-        
-class marstermind():
+
+
+class marstermind:  # du coup Mastermind dans le fichier mastermind.py
+
     """
     Classe du jeu
     """
+
     pass
-    
+
     def afficher_couleurchoisi(self):
         """
-        Classe choix aléatoire de la couleur        
+        Classe choix aléatoire de la couleur
         """
         pass
-    
-    afficher_couleurchoisi.__doc__
-    help(afficher_couleurchoisi)
-    
+
+    afficher_couleurchoisi.__doc__  # ? Faites vous un fichier a coté vouspolluez beaucoup le code
+    help(afficher_couleurchoisi)  # ? ou alors en bas ou dans uen fonction a part
+
     def result_secret(self):
-        
+
         pass
-    
-    result_secret.__doc__
-    help(result_secret)
-    
-    def evaluation_code(self,code_secret, code_entree):
+
+    result_secret.__doc__  # idem
+    help(result_secret)  # idem
+
+    def evaluation_code(self, code_secret, code_entree):  # ne sert a rien
         """
         Evalution combinaisn
-        secret.
+        secret. # ? que voulez vous dire ?
         """
-        self.position = (code_secret, code_entree)
+        self.position = (code_secret, code_entree)  # ne veut rien dire.
+
     def __str__(self):
-        return f"code indéfinie de position {self.position}" 
-    
-    evaluation_code.__doc__ 
-    help(evaluation_code)
-    
+        return f"code indéfinie de position {self.position}"
+
+    evaluation_code.__doc__a  # idemt
+    help(evaluation_code)  #
+
+
+# if __name__ == '__main__'  ?
 def main():
-    print ("Bienvenue")
-    if __name__ == '__main__':
+    print("Bienvenue")
+    if __name__ == "__main__":
         doctest.testmod()
+
+
 main()
-        
+
 # doctest hashed values
-couleurs = ['bleu','rose', 'or','argent']
-print('dict:', {x: len(x) for x in couleurs})
-print('set :', set(couleurs))
+couleurs = ["bleu", "rose", "or", "argent"]
+print("dict:", {x: len(x) for x in couleurs})
+print("set :", set(couleurs))
 
 # doctest private tests
 __test__ = {
-    'numbers': """
->>> evaluation_code('br','br') 
+    "numbers": """
+>>> evaluation_code('br','br')
 [2, 2]
 
 >>> evaluation_code('br','bo')
 [1, 1]
 """,
-
-    'external': doctest_private_tests_external,
+    "external": doctest_private_tests_external,
 }
+# ???? Que voules vous faire
 
 ################################################   Tests unitaires ################################################
-    
+
 import unittest
 
-# unittest truth 
-class jeu(unittest.TestCase):
-
+# unittest truth
+class jeu(unittest.TestCase):  # TestJeu
     def testAssertTrue(self):
-        self.assertTrue(True, ' Vous avez gagné le jeu :) ')
+        self.assertTrue(True, " Vous avez gagné le jeu :) ")
 
     def testAssertFalse(self):
-        self.assertFalse(False, ' vous avez perdu le jeu :(')
-        
+        self.assertFalse(False, " vous avez perdu le jeu :(")
+
 
 ################################################ Autre tests ################################################
 
+# les tests sont automatiques .... ou alors a part mais pas a chaque import du module
+# limit dans if __name__ == '__main___':
 code_secret = input(" entrer un code secret : ")
 pions = input(" entrer des pions : ")
-if code_secret == pions :
-    print( " mot de passe trouvé ! " )
-else : 
+if code_secret == pions:
+    print(" mot de passe trouvé ! ")
+else:
     print("Vous avez pas trouvé le code secret ! ")
 
 
-
 # La fonction shuffle est utilisé pour choisir une couleur aléatoire.
+# Non c'est random.choice, shuffle renvoie un liste melangée
 liste_couleurs = list(couleurs)
 shuffle(liste_couleurs)
 print(couleurs, liste_couleurs)
 
 x = 0
 for valeur in couleurs:
-    if liste_couleurs[x] == valeur :
-        print(" pion ", valeur,  " à la place ", x)
+    if liste_couleurs[x] == valeur:
+        print(" pion ", valeur, " à la place ", x)
     else:
         print(" pions à la place ", x)
 
     x += 1
-
